79f293b6e4d8d24be23b2a31bb19d87a78a19782
xiaocai <1766607952@qq.com>
2021-06-04 10:26:42 +0800

    目录结构调整

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <div class="nav">
       黑马头条
+      <button>乞丐按钮</button>
+      <van-button type="primary">主要按钮</van-button>
     </div>
     <router-view />
   </div>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vant from 'vant' // 组件库
+import 'vant/lib/index.css' // 样式
+
+Vue.use(Vant) // 引入
 Vue.config.productionTip = false
 
 new Vue({
