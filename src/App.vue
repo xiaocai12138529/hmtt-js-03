c9d7e84526732e318eea3e267960659ece6dcbd9
xiaocai <1766607952@qq.com>
2021-06-04 09:35:01 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SideBar />
+    <div style="display: flex; height: 100%">
+      <NavBar />
+      <div style="flex: 1 1 auto; border: 1px solid #ccc; text-align: center">
+        主体
+        <button @click="btnImg">改变图片</button>
+        <p>
+          <input type="number" min="1" v-model="$store.state.age" />
+          {{ $store.getters.getAge }}
+        </p>
+        <div v-for="(item, i) in $store.state.data" :key="i">
+          书本:{{ item.name }} 
+          价格:{{ item.price }}
+        </div>
+        和:{{$store.getters.getPrice}}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SideBar from './views/SideBar'
+import NavBar from './views/NavBar'
+// import axios from 'axios'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    NavBar, SideBar
+  },
+  methods: {
+    btnImg () {
+      const newUrl = 'http://s02.mifile.cn/assets/static/image/logo-mi2.png'
+      this.$store.commit('setUrl', newUrl)
+    }
+  },
+  // 发送ajax请求存入vuex
+  created () {
+    // axios({
+    //   url: 'https://www.fastmock.site/mock/37d3b9f13a48d528a9339fbed1b81bd5/book/api/books'
+    // }).then(res => {
+    //   console.log(res),
+    //     this.$store.commit('setData', res.data.data)
+    // })
+    // this.$storestore.dispatch('action', payload)
+    this.$store.dispatch('getBook')
   }
+
 }
 </script>
 
 <style>
+body,
+html,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
